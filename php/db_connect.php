deed36c2f7b5176257d4ca88394cc4718e21759f
letloleT <letloletshepang@gmail.com>
2016-01-30 03:35:55 +0200

    Added the db connection

---
+++ php/db_connect.php
@@ -0,0 +1,9 @@
+<?php 
+$dbhost = '10.0.19.1';
+$dbuser = 'mat03490';
+$dbpass = 'mat03490';
+$dbname = 'db_mat03490';
+mysql = mysql_connect('10.0.19.1','mat03490','mat03490');
+                                mysql_select_db("db_mat03490");
+								
+?>
